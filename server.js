9783f51904203e8b3557e79e8265c09cbd10e7bf
Adam Peterka <peterkaa@gmatc.matc.edu>
2019-10-20 13:26:06 -0500

    Completed Activity 5

--- server.js
+++ server.js
@@ -75,7 +75,23 @@ app.post(
 
         //Save to the db and return
         await user.save();
-        res.send('User successfully registered');
+
+        //Generate and return a JWT Token
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          { expiresIn: '10hr' },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token: token });
+          }
+        );
       } catch (error) {
         res.status(500).send('Server error');
       }
