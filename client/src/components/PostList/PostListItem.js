47987fcfc38c6254d212706459e11793e3d94750
Adam Peterka <peterkaa@gmatc.matc.edu>
2019-12-01 12:24:52 -0600

    fixed errors

--- client/src/components/PostList/PostListItem.js
+++ client/src/components/PostList/PostListItem.js
@@ -4,19 +4,19 @@ import slugify from "slugify";
 import "./styles.css";
 
 const PostListItem = props => {
-  const { post, clickpost } = props;
+  const { post, clickPost } = props;
   const history = useHistory();
 
   const handleClickPost = post => {
     const slug = slugify(post.title, { lower: true });
 
-    clickpost(post);
+    clickPost(post);
     history.push(`/posts/${slug}`);
   };
 
   return (
     <div>
-      <div className="postListIten" onClick={() => handleClickPost(post)}>
+      <div className="postListItem" onClick={() => handleClickPost(post)}>
         <h2>{post.title}</h2>
         <p>{post.body}</p>
       </div>
